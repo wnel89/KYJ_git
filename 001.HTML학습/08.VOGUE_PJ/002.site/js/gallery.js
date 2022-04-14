058957c94a49e27f02949a384f6027c729dbef12
wnel89 <wnel8977@gmail.com>
2022-04-14 17:18:49 +0900

    12123

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/gallery.js
@@ -1,1 +1,21 @@
 //갤러리 페이지 JS  - gallery.js
+
+$(() => { /////////// JQB ////////////
+
+    console.log("로딩완료");
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}) /////////// JQB ////////////
